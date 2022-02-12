1a76b417d42a2614e950e5817bfdd151df36661c
Félix <felix.buisson@epitech.eu>
2022-02-12 21:33:11 +0100

    fix Buton position & add signal achievement Carton

--- Carton/Carton.gd
+++ Carton/Carton.gd
@@ -4,7 +4,9 @@ var unpack = ["deadCat", "aliveCat", "airplane", "jam"] #add XddL w/ signal
 var open = 0
 var upk = 0
 var remove
+var func_called = true
 
+signal achievement
 signal empty
 
 func _on_CartonBody_clickBox():
@@ -12,10 +14,17 @@ func _on_CartonBody_clickBox():
 		print("I think there is nothing left to unpack.")
 		emit_signal("empty")
 	elif !unpack.empty():
-		open = randi() % 4
-		if open == 1:
-			remove = unpack.pop_at(randi() % (unpack.size()))
-			print(remove)
-			upk += 1
+		if func_called:
+			open = randi() % 4
+			if open == 1:
+				remove = unpack.pop_at(randi() % (unpack.size()))
+				print(remove)
+				upk += 1
+				emit_signal("achievement")
+				func_called = false
 		else:
 			print("The box is empty...")
+
+func return_unpacked_object():
+	func_called = true
+	return remove
