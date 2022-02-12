1f1a7b881a40673496f31bdf39f4c66d07e0e77b
Briag FROUX ROTRU <briag.froux@epitech.eu>
2022-02-12 23:23:36 +0100

    Update GameScreen & fix button & json

--- Bouton/Bouton.gd
+++ Bouton/Bouton.gd
@@ -1,8 +1,31 @@
 extends Control
 
 var pressed = 0
-var ach = ["ach1", "ach2", "ach3", "ach4", "ach5", "ach6", "ach7", "ach8", "ach9", "ach10", "ach11", "ach12",
-			"ach13", "ach14", "ach15", "ach16", "ach17", "ach18", "ach19", "ach20", "ach42", "ach50", "ach999"]
+var ach = [
+	"vous_avez_appuyez_1_fois_sur_le_bouton",
+	"vous_avez_appuyez_2_fois_sur_le_bouton",
+	"vous_avez_appuyez_3_fois_sur_le_bouton",
+	"vous_avez_appuyez_4_fois_sur_le_bouton",
+	"vous_avez_appuyez_5_fois_sur_le_bouton",
+	"vous_avez_appuyez_6_fois_sur_le_bouton",
+	"vous_avez_appuyez_7_fois_sur_le_bouton",
+	"vous_avez_appuyez_8_fois_sur_le_bouton",
+	"vous_avez_appuyez_9_fois_sur_le_bouton",
+	"vous_avez_appuyez_10_fois_sur_le_bouton",
+	"vous_avez_appuyez_11_fois_sur_le_bouton",
+	"vous_avez_appuyez_12_fois_sur_le_bouton",
+	"vous_avez_appuyez_13_fois_sur_le_bouton",
+	"vous_avez_appuyez_14_fois_sur_le_bouton",
+	"vous_avez_appuyez_15_fois_sur_le_bouton",
+	"vous_avez_appuyez_16_fois_sur_le_bouton",
+	"vous_avez_appuyez_17_fois_sur_le_bouton",
+	"vous_avez_appuyez_18_fois_sur_le_bouton",
+	"vous_avez_appuyez_19_fois_sur_le_bouton",
+	"vous_avez_appuyez_20_fois_sur_le_bouton",
+	"la_reponse_a_la_vie,_a_l'univers_et_tout_le_reste",
+	"tu_t'es_crus_sur_cookie_clicker_?",
+	"le_triple_neuf"
+]
 var count = -1
 
 signal achievement
--- Game/GameScreen.gd
+++ Game/GameScreen.gd
@@ -4,8 +4,180 @@ onready var Telefax = get_node("Telefax")
 onready var Minitel = get_node("Minitel")
 onready var Carton = get_node("Carton")
 
+var toast_count = 0
+var success_count = 0
+
 func _ready():
 	randomize()
 
 func _process(delta):
-	pass
+	var toast
+	if toast_count == 7:
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("trop_de_toast_tue_le_toast", true)
+		toast_count += 1
+		success_count += 1
+	if success_count == 50:
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("serious_dedication", true)
+		toast_count += 1
+		success_count += 1
+	elif success_count == 100:
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("how_did_we_get_here?", true)
+		toast_count += 1
+		success_count += 1
+
+func toast_end(node):
+	node.queue_free()
+	toast_count -= 1
+
+func _on_Carton_empty():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("unpacked", true)
+	toast_count += 1
+	success_count += 1
+	get_node("Carton").queue_free()
+
+func _on_Minitel_alphabet():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("le_larousse", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_briag():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("briag", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_fanta():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("salut_a_tous,_c'est_fanta", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_felix():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("felix", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_godot():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("3615_godot", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_gorille():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("monke", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_malo():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("malo", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_monique():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("ha_bah_quelle_productivite", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_noe():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("noe", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_usul():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("3615_usul", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Minitel_veronique():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement("veronique_nique_nique", true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Bouton_achievement():
+	get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+	var toast = get_node("ToastsContainer").get_child(toast_count)
+	toast.connect("finished", self, "toast_end")
+	toast.modify_achievement(get_node("Bouton").get_achievement(), true)
+	toast_count += 1
+	success_count += 1
+
+func _on_Carton_achievement():
+	var toast
+	var object = get_node("Carton").return_unpacked_object()
+	if object == "deadCat":
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("schrodinger_cat_is_a_lie", true)
+		toast_count += 1
+		success_count += 1
+	elif object == "aliveCat":
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("schrodinger_cat_is_alive", true)
+		toast_count += 1
+		success_count += 1
+	elif object == "airplane":
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("mh370", true)
+		toast_count += 1
+		success_count += 1
+	elif object == "jam":
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("vous_l'avez,_non_?", true)
+		toast_count += 1
+		success_count += 1
+	else:
+		get_node("ToastsContainer").add_child(load("res://Toasts/Toast.tscn").instance())
+		toast = get_node("ToastsContainer").get_child(toast_count)
+		toast.connect("finished", self, "toast_end")
+		toast.modify_achievement("maintenant,_oui", true)
+		toast_count += 1
+		success_count += 1
