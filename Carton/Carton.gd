141e0897c8d08c88bfac396d46b1ca6fe3bad636
Félix <felix.buisson@epitech.eu>
2022-02-13 21:50:38 +0100

    unapcked complete

--- Carton/Carton.gd
+++ Carton/Carton.gd
@@ -26,11 +26,25 @@ func _on_CartonBody_clickBox():
 		if func_called and open == 1:
 			remove = unpack.pop_at(randi() % (unpack.size()))
 			print(remove)
+			display_unpack(remove)
 			upk += 1
 			func_called = false
 			emit_signal("achievement")
 		else:
 			print("The box is empty...")
+
+func display_unpack(remove):
+	timer.start()
+	if remove == "XDdL":
+		get_node("Sprite").show()
+	if remove == "aliveCat":
+		get_node("Sprite2").show()
+	if remove == "jam":
+		get_node("Sprite3").show()
+	if remove == "airplane":
+		get_node("Sprite4").show()
+	if remove == "deadCat":
+		get_node("Sprite5").show()
 
 func return_unpacked_object():
 	func_called = true
@@ -39,3 +53,8 @@ func return_unpacked_object():
 
 func _on_Timer_timeout():
 	get_node("CartonBody/Sprite").frame = 0
+	get_node("Sprite").hide()
+	get_node("Sprite2").hide()
+	get_node("Sprite3").hide()
+	get_node("Sprite4").hide()
+	get_node("Sprite5").hide()
