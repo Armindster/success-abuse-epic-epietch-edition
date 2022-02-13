e27ab927caf88e8cd76d796810fb57b409f29f60
Félix <felix.buisson@epitech.eu>
2022-02-13 02:03:45 +0100

    airbag de sécurité pour pas spam les achievement du Tableau

--- Tableau/Tableau.gd
+++ Tableau/Tableau.gd
@@ -5,10 +5,13 @@ signal Quote
 signal Jeff
 
 func _on_Area2DIsaac_clickIsaac():
+	$Area2DIsaac/CollisionShape2D.disabled = true
 	emit_signal("Isaac")
 
 func _on_Area2DQuote_clickQuote():
+	$Area2DQuote/CollisionShape2D.disabled = true
 	emit_signal("Quote")
 
 func _on_Area2DJeff_clickJeff():
+	$Area2DJeff/CollisionShape2D.disabled = true
 	emit_signal("Jeff")
